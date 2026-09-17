d4ec5258b4fd1691c2407d9d17cf5269e8f8ba8c
Reaper754 <MasonT019@stemcollegiate.ca>
2026-09-17 10:51:37 -0600

    v

--- main.py
+++ main.py
@@ -6,4 +6,72 @@ cat = Pet("Fluffy", 50, "Cat", "Worms")
 ferret = Pet("Buck", 75, "Ferret", "Won't eat")
 vet = Veterinarian()
 
-select =
+select = """Choose your patient:
+1 for Max | 2 for Fluffy | 3 for Buck | 4 for exit\n"""
+
+message = """Enter an input: 
+| Checkup | Treat |\n"""
+
+commands = [
+    "checkup",
+    "treat",
+]
+
+actions1 = [
+    vet.checkup,
+    vet.treat,
+]
+
+actions2 = [
+    vet.checkup,
+    vet.treat,
+]
+
+actions3 = [
+    vet.checkup,
+    vet.treat,
+]
+
+while True:
+    try:
+        patient = int(input(select))
+        print("type exit to exit.")
+        if patient == 1:
+            user_input = input(message).strip().lower()
+            for command, action in zip(commands, actions1):
+                if user_input == command:
+                    if command == "treat":
+                        action()
+                    action()
+                    break
+                else:
+                    if user_input == "exit":
+                        break
+                    print("Invalid command.")
+        elif patient == 2:
+            user_input = input(message).strip().lower()
+            for command, action2 in zip(commands, actions2):
+                if user_input == command:
+                    action2(cat)
+                    break
+                else:
+                    if user_input == "exit":
+                        break
+                    print("Invalid command.")
+        elif patient == 3:
+            user_input = input(message).strip().lower()
+            for command, action3 in zip(commands, actions3):
+                if user_input == command:
+                    action3(ferret)
+                    break
+                else:
+                    if user_input  == "exit":
+                        break
+                    print("Invalid command.")
+        else:
+            if patient == 4:
+                break
+            print("Invalid command.")
+    except ValueError:
+        print("Invalid input")
+
--- veterinarian.py
+++ veterinarian.py
@@ -7,5 +7,10 @@ class Veterinarian:
     def checkup(self, pet):
         print(f"File: Name: {pet.name}. Health: {pet.health}%. Species: {pet.species}. Symptom: {pet.symptom}")
 
-    def treat(self, Pet, amount):
-        pass
+    def treat(self, pet, amount):
+        if pet.name == "Max":
+            print(f"{self.name} healed Max and it turns out he was faking his illness to get attention. Gained 200 dollars.")
+            self.money += 200
+            print(f"Your health: {self.health}, your money: {self.money}")
+        if pet.name == "Fluffy":
+            pass
